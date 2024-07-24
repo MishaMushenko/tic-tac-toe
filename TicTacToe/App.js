5567293030bc2cb573347c9541858c9d6a4ac04b
MishaMushenko <miivan151515@gmail.com>
2024-07-24 19:50:33 +0300

    Update App.js

--- TicTacToe/App.js
+++ TicTacToe/App.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
-import {View, Text, TouchableOpcaty, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Mobal, Button } from 'rect-native';
 
 export default function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setlsXNext] = useState(true);
-  const handlePress = (index) => {
-    const newBoard = board.slice();
-    if(calculateWinner(board) || board[index]) {
-      return;
-    }
-    newBoard[index] = isXNext ? 'X' : 'O';
-    setlsXNext(!isXNext);
-  };
+  const [board, setBoard] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ]);
+  const [currentPlaer, setCurrentPlaer] =useState('X');
+  const [winner, setWinner] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const handlePress = (row, col) => {
+  if (board[row][col] !==''|| winner) return;
+  const newBoard = board.map((r,i) =>
+   r.map((cell,j) => {
+    if (i === row && j === col) return currentPlayr;
+   })
+  );
+  setboard(newBard);
+  const win = checkWinner(newBoard);
+  if(win) {
+    setWinner(currentPlayer);
+    setModalVisible(true);
+  } else {
+    setCurrentPlayer(currentPlaeyr === 'X' ? 'O':'X');
+  }
+};
